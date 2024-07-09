52e52bf75a315cfc650a8955919f6b44c82915a8
mirafuentesjenalyn <mirafuentesjenalyn@gmail.com>
2024-07-09 17:38:08 +0800

    aboutme-box

--- jen/src/components/Home.js
+++ jen/src/components/Home.js
@@ -21,7 +21,18 @@ function Home() {
       </div>
       <div className='aboutme-section'>
           <h2 className="aboutme-title"><Link to="/profile"> About Me </Link><div className='underline'></div></h2>
-          <p> I am a Computer Engineer with  passion for continuous learning in coding and design. I am dedicated in developing user-friendly and visually appealing software solutions, bringing creativity and technical expertise to every project, and striving to deliver impactful results. </p>
+            <div className="aboutme-text">
+            <p>
+              <span className="styled-text">I am a </span>
+              <span className="styled-highlight">Computer Engineer </span>
+              <span className="styled-text">with a passion for continuous learning in </span>
+              <span className="styled-highlight">coding </span>
+              <span className="styled-text">and </span>
+              <span className="styled-highlight">design</span>
+              <span className="styled-text">. </span>
+              <span className="styled-text">I am dedicated to developing user-friendly and visually appealing software solutions, bringing creativity and technical expertise to every project, and striving to deliver impactful results.</span>
+            </p>
+          </div>
           </div>
     </div>
   );
